288e608592f86ee47434050a6b6bf31bf574a5d7
thruse <thruse7@gmail.com>
2025-08-20 16:28:58 +0100

    pushing to game cloud operational!

--- gamecloud.py
+++ gamecloud.py
@@ -2,29 +2,37 @@
 
 import os
 import sys
+import shutil
 
 schemas_dir = os.path.join(os.path.dirname(__file__), "schemas")
-schemas = os.listdir(schemas_dir)
+saves_dir = os.path.join(os.path.dirname(__file__), "saves")
+os.makedirs(saves_dir, exist_ok=True)
+games = os.listdir(schemas_dir)
+for game in games:
+    os.makedirs(os.path.join(saves_dir, game), exist_ok=True)
 
 commands = ["push", "pull"]
-if len(sys.argv) == 3 and sys.argv[1] in commands and sys.argv[2] in schemas:
+if len(sys.argv) == 3 and sys.argv[1] in commands and sys.argv[2] in games:
     command = sys.argv[1]
-    schema_file = os.path.join(schemas_dir, sys.argv[2])
+    game = sys.argv[2]
+    game_save_dir = os.path.join(saves_dir, game)
+    
+    schema_file = os.path.join(schemas_dir, game)
     if command == "push":
         with open(schema_file) as f:
             schema_lines = f.readlines()
         
         schema = [line.strip() for line in schema_lines]
         if sys.platform == "win32":
-            save_dir = os.path.expandvars(schema[1])
+            local_save_dir = os.path.expandvars(schema[1])
         elif sys.platform == "darwin":
-            save_dir = os.path.expandvars(schema[2])
+            local_save_dir = os.path.expandvars(schema[2])
         elif sys.platform == "linux":
-            save_dir = os.path.expandvars(schema[3])
+            local_save_dir = os.path.expandvars(schema[3])
         
-        save_file_paths = [os.path.join(save_dir, file_name) for file_name in schema[4:]]
-        for path in save_file_paths:
-            print(path, "is the path to a save file and it is", os.path.exists(path), "that the file exists")
+        local_save_files = [os.path.join(local_save_dir, file_name) for file_name in schema[4:] if os.path.exists(os.path.join(local_save_dir, file_name))]
+        for local_save_file in local_save_files:
+            shutil.copy(local_save_file, game_save_dir)
     elif command == "pull":
         print("pulling")
 else:
