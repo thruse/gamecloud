bc085edca1c0b5688c1b50db3a3fec72effd1699
thruse <thruse7@gmail.com>
2025-08-26 15:35:24 +0100

    renamed `real_path` in `dirname.bat` to `dir_name`.

--- bin/dirname.bat
+++ bin/dirname.bat
@@ -1,7 +1,7 @@
 @echo off
 set full_path=%~f1
-for %%i in ("%full_path%") do set real_dir=%%~dpi
-echo %real_dir:~0,-1%
+for %%i in ("%full_path%") do set dir_name=%%~dpi
+echo %dir_name:~0,-1%
 
 set full_path=
-set real_dir=
+set dir_name=
